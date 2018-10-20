622c70ca9bd874299d89ddaa0a35651f25ba1bb2
Lucas <lucas.lima@mblabs.com.br>
2018-10-20 11:46:44 -0300

    profile fragment mvp

--- app/src/main/java/com/example/lucas/deliva/presentation/order/presenter/OrderProfileFragmentPresenter.java
+++ app/src/main/java/com/example/lucas/deliva/presentation/order/presenter/OrderProfileFragmentPresenter.java
@@ -1,6 +1,13 @@
 package com.example.lucas.deliva.presentation.order.presenter;
 
 import com.example.lucas.deliva.presentation.base.presenter.BasePresenter;
+import com.example.lucas.deliva.presentation.order.view.OrderProfileFragment;
 
 public class OrderProfileFragmentPresenter extends BasePresenter {
+
+    private OrderProfileFragment mView;
+
+    public OrderProfileFragmentPresenter(OrderProfileFragment view) {
+        this.mView = view;
+    }
 }
--- app/src/main/java/com/example/lucas/deliva/presentation/order/view/OrderProfileFragment.java
+++ app/src/main/java/com/example/lucas/deliva/presentation/order/view/OrderProfileFragment.java
@@ -6,21 +6,34 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 
 import com.example.lucas.deliva.R;
 import com.example.lucas.deliva.presentation.base.presenter.BasePresenter;
 import com.example.lucas.deliva.presentation.base.view.BaseFragment;
 import com.example.lucas.deliva.presentation.base.view.adapter.OrderTabsViewPagerAdapter;
 import com.example.lucas.deliva.presentation.base.view.AdapterAwareFragment;
+import com.example.lucas.deliva.presentation.order.presenter.OrderProfileFragmentPresenter;
 
 import butterknife.BindView;
 
-public class OrderProfileFragment extends BaseFragment<BasePresenter> {
+public class OrderProfileFragment extends BaseFragment<OrderProfileFragmentPresenter> implements OrderProfileFragmentView {
+
+    @BindView(R.id.total)
+    protected TextView mTotal;
+
+    @BindView(R.id.accommodation)
+    protected TextView mAccomodation;
+
+    @BindView(R.id.recycle_view)
+    protected RecyclerView mRecyclerView;
+
 
     @NonNull
     @Override
-    protected BasePresenter createPresenter(@NonNull Context context) {
-        return BasePresenter.nullPresenter(context);
+    protected OrderProfileFragmentPresenter createPresenter(@NonNull Context context) {
+        return new OrderProfileFragmentPresenter(this);
     }
 
     @Override
@@ -31,6 +44,11 @@ public class OrderProfileFragment extends BaseFragment<BasePresenter> {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
     }
+
+
+
+
 
 }
